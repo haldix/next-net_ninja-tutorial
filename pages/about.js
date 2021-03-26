26f8526b9d72e50822ce6aa563505327c2f2b112
haldix <hjdpirate@yahoo.com>
2021-03-26 16:05:10 -0400

    finished tutorial

---
+++ pages/about.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import Head from 'next/head';
+
+const about = () => {
+  return (
+    <>
+      <Head>
+        <title>Ninja List | About</title>
+        <meta name='content' keywords='Ninjas' />
+      </Head>
+      <div>
+        <h1>About</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. At aliquid,
+          exercitationem possimus minima cumque omnis, vitae libero itaque
+          debitis autem nam voluptatibus labore quasi veritatis quo sint ipsam
+          nihil amet pariatur? Mollitia amet sint, maxime corporis repudiandae
+          commodi minus cum?
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. At aliquid,
+          exercitationem possimus minima cumque omnis, vitae libero itaque
+          debitis autem nam voluptatibus labore quasi veritatis quo sint ipsam
+          nihil amet pariatur? Mollitia amet sint, maxime corporis repudiandae
+          commodi minus cum?
+        </p>
+      </div>
+    </>
+  );
+};
+
+export default about;
